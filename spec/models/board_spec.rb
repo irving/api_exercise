0a0c50bc7758ae7231d438b3e2d8fe2ff6fa1610
Dave Vandervort <drvandervort@gmail.com>
2018-11-17 20:49:13 -0500

    Improved testing and created boards

---
+++ spec/models/board_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Board, type: :model do
+  let(:valid_params) { { name: 'test' } }
+
+  context "validations" do
+    it { expect(described_class.new).not_to be_valid }
+    it { expect(described_class.new valid_params).to be_valid }
+
+    it "fails on duplicate name" do
+      create :board
+      expect(build(:board)).not_to be_valid
+    end
+  end
+end
